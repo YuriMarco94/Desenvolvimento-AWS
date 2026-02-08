6a0e4f142da4212aed78fe08efbad39814e0c631
YuriMarco94 <yurimarconruschel@outlook.com>
2026-02-07 21:42:27 -0300

    chore(terraform): configure remote backend for dev/hom/prod

---
+++ terraform/envs/prod/main.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "yurim-aws-platform-us-east-1-tfstate-0dc5ad"
+    key            = "env/prod/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "yurim-aws-platform-us-east-1-tflock"
+    encrypt        = true
+  }
+}
